d346fc5cda1ba675133bd7c1cece5cd7e0e71930
huchu <jso0330@sch.ac.kr>
2021-07-22 17:33:32 +0900

    index 수정

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<br><br><h1 align=center>SMART GARDEN</h1>
+
+<table align=center>
+  <tr><td><a href="code/view.php" style='text-decoration:none'>임시 게시판</a></td></tr>
+  <tr><td><hr width=300></td></tr>
+  <tr><td><a href="pages/ethernet_view.php" style='text-decoration:none'>데이터 조회 1</a></td></tr>
+  <tr><td><hr width=300></td></tr>
+  <tr><td><a href="pages/icon_view.php" style='text-decoration:none'>데이터 조회 2</a></td></tr>
+  <tr><td><hr width=300></td></tr>
+  <tr><td><a href="pages/relay.php" style='text-decoration:none'>펌프 동작 테스트</a></td></tr>
+</table>
